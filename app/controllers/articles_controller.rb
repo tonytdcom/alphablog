0142932b9d4ce18e12e013009a28c18fd617680a
TDCOM <tony@tdcom.co.uk>
2017-02-22 06:41:03 +0000

    edit, update etc

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,33 @@
 class ArticlesController < ApplicationController
+   def index
+       @articles = Article.all
+   end
+   
+   
     def new
         @article = Article.new
     end
+    def edit
+       @article = Article.find(params[:id]) 
+    end
+    
     
     def create 
         
         #render plain: params[:article].inspect
         @article = Article.new(article_params)
-        @article.save
+        if @article.save
+            flash[:notice] = "Article saved"
+            
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
         
     end
     
     def show
-        
+        @article = Article.find(params[:id])
     end
     
     def destroy
@@ -20,7 +35,14 @@ class ArticlesController < ApplicationController
     end
     
     def update
-        
+       @article = Article.find(params[:id])
+       if @article.update(article_params)
+           flash[:notice] ="Updated successfully"
+           redirect_to article_path(@article)
+       else
+           render 'edit'
+       end
+       
     end
     
     private
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
    
-   validates :title, presence: true
-   validates :description, presence: true
+   validates :title, presence: true 
+   validates :description, presence: true 
     
 end
 
