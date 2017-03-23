981bb01d37ee811f49deb5807cedbf50ff50c943
TDCOM <tony@tdcom.co.uk>
2017-03-23 17:59:41 +0000

    add user styling

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,8 @@
 class Article < ActiveRecord::Base
-   
+   belongs_to :user
    validates :title, presence: true 
    validates :description, presence: true 
-   
+   validates :user_id, presence: true
    def self.search(search)
    where("title LIKE ? OR description LIKE ?", "%#{search}%", "%#{search}%") 
    end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307212803) do
+ActiveRecord::Schema.define(version: 20170322215533) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
