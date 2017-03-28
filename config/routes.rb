5af3b77cf3502527e3d62aa9b8f7ab02b9f30b13
TDCOM <tony@tdcom.co.uk>
2017-03-28 08:15:08 +0000

    user logon working

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,10 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
   root 'pages#home'
   get 'about', to: 'pages#about'
+  get 'signup', to: 'users#new'
+  
   resources :articles
+  resources :users, except:['new']
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
