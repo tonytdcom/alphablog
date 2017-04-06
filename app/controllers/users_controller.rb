81ebf7a3ea2798a5fb772b204d11a1fba0982ab8
TDCOM <tony@tdcom.co.uk>
2017-04-06 06:43:19 +0000

    users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,22 @@ class UsersController < ApplicationController
         render "new"  
     end
   end
+  def edit
+        
+        @user = User.find(params[:id])
+        
+  end
+    
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:success] = "Your account was updated"
+        redirect_to articles_path
+      else
+        render 'edit'
+      end
+
+    end
   
   private
   
