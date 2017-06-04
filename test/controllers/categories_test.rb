4e0370d421dbdd4c0a025c98c32dfbd1738a18c5
TDCOM <tony@tdcom.co.uk>
2017-06-04 11:43:59 +0000

    add user.admin

--- test/controllers/categories_test.rb
+++ test/controllers/categories_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class CategoriesControllerTest < ActionController::TestCase
     def setup
         @category = Category.create(name: "cars")
+        @user = User.create(username: "john", email: "john@john.com", password: "password", admin: True)
     end
 
 
@@ -13,6 +14,7 @@ class CategoriesControllerTest < ActionController::TestCase
     end
     
     test " should get new" do
+      session[:user_id] = @user.id
       get :new
       assert_response :success
     end
@@ -20,6 +22,13 @@ class CategoriesControllerTest < ActionController::TestCase
     test " should get show" do
       get(:show, {'id' => @category.id})
       assert_response :success
+    end
+    
+    test "should redirect when admin not logged in" do
+        assert_no_difference 'Category.count' do
+        post :create, category: { name: "sports" }
+        end
+    assert_redirected_to categories_path
     end
     
 end
